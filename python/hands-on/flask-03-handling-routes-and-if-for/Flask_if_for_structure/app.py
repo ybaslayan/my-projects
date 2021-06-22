0e54e562793ec5403e28fcfbaf485cdc5763dfe2
ybaslayan <ybaslayan@gmail.com>
2021-06-22 22:53:18 +0300

    Last chanced2

--- python/hands-on/flask-03-handling-routes-and-if-for/Flask_if_for_structure/app.py
+++ python/hands-on/flask-03-handling-routes-and-if-for/Flask_if_for_structure/app.py
@@ -28,7 +28,7 @@ def header():
 
 if __name__ == '__main__':
     # app.run(debug=True)
-    app.run(host=‘0.0.0.0’, port=80)
+    app.run(host='0.0.0.0', port=80)
 
 
 
--- python/hands-on/flask-03-handling-routes-and-if-for/Handling-routes/app.py
+++ python/hands-on/flask-03-handling-routes-and-if-for/Handling-routes/app.py
@@ -93,4 +93,4 @@ def evens():
 
 if __name__ == '__main__':
     # app.run(debug=True)
-    app.run(host=‘0.0.0.0’, port=80)
+    app.run(host='0.0.0.0', port=80)
